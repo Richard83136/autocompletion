eb67a4a51c46972963ae383832d14affcd7c1b53
richard-schulze <richard.schulze@laplateforme.io>
2023-04-13 15:06:37 +0200

    ajout fichier dbconnect

---
+++ dbconnect.php
@@ -0,0 +1,72 @@
+<?php
+
+class Database{   
+
+    public function __construct(){
+        
+        $host = "localhost";
+        $db_name = "autocompletion";
+        $username = "root";
+        $password = "";
+        $conn = null;
+    
+        try
+            {
+                $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            }
+            catch(PDOException $exception)
+            {
+                echo "Connection error: " . $exception->getMessage();
+            }
+        }
+    }
+  
+    
+    function detail_product(){
+        $host = "localhost";
+        $db_name = "autocompletion";
+        $username = "root";
+        $password = "";
+        $conn = null;
+        $request = "SELECT * FROM film WHERE id = :id";
+        $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $stmt = $conn->prepare($request);
+        $stmt->execute([
+            ':id' => $_GET['id']
+        ]);
+        $product = $stmt->fetch();
+        return $product;
+    }
+
+    function getlist(){
+        $host = "localhost";
+        $db_name = "autocompletion";
+        $username = "root";
+        $password = "";
+        $conn = null;
+        $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        if (isset($_GET['search'])){   
+            
+            $sql = "SELECT * FROM film WHERE nom LIKE :term  ";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([
+                ':term' => $_GET['search'] . '%'
+            ]);
+            $suggestion = $stmt->fetchAll();
+            foreach($suggestion as $produit){
+                    echo '<li class="listsearch"><a href="element.php?id='.$produit['id'].'">'.$produit['nom'].'</a></li>';
+            }
+        }
+   
+        else{
+            echo 'Aucun film ne correspond a votre recheche';
+        }
+    }
+  
+?>
